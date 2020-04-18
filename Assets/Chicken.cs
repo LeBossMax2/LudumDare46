9282877144370ad8c363c9da34ebd2c44fc79b51
Lucas Guedon <guedon.lucas@laposte.net>
2020-04-18 16:43:29 +0200

    Add game over screen

---
+++ Assets/Chicken.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chicken : MonoBehaviour
+{
+    public Sprite deadSprite;
+
+    public void OnDeath()
+    {
+        GetComponent<SpriteRenderer>().sprite = deadSprite;
+    }
+}
--- Assets/KillTarget.cs
+++ Assets/KillTarget.cs
@@ -1,14 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class KillTarget : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("target"))
+        OnCollider(collision.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        OnCollider(other.gameObject);
+    }
+
+    private void OnCollider(GameObject obj)
+    {
+        if (obj.CompareTag("target"))
         {
-            //GAME OVER
+            Chicken ch = obj.GetComponent<Chicken>();
+
+            if (ch != null)
+                ch.OnDeath();
+
+            Camera cam = Camera.main;
+            cam.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y, cam.transform.position.z);
+            cam.orthographicSize = 4;
+            Time.timeScale = 0;
+            SceneManager.LoadScene("GameOver", LoadSceneMode.Additive);
         }
     }
 }
